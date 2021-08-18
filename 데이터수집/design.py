46c0b626f9e82d1d1aafdf2fc5fae71703e6d18a
pterons <2pterons@gmail.com>
2021-08-18 16:39:51 +0900

    데이터 수집_design사이트_자동 페이지

---
+++ 데이터수집/design.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+magazineOffset = 0
+contestOffset = 0
+exhibitOffset = 0
+galleryOffset = 0
+
+for i in range(10): # 0~9
+    params = {
+    #magazineOffset=0&contestOffset=0&exhibitOffset=0&galleryOffset=0
+    'magazineOffset':magazineOffset
+    ,'contestOffset':contestOffset
+    ,'exhibitOffset':exhibitOffset
+    ,'galleryOffset':galleryOffset
+    }
+
+    response = requests.get("https://www.jungle.co.kr/recent.json", params=params)
+    print(response.text)
+
+    magazineOffset = data['magazineOffset'] #0
+    contestOffset = data['contestOffset'] #6
+    exhibitOffset = data['exhibitOffset'] #0
+    galleryOffset = data['galleryOffset'] #0
+
+    time.sleep(1) #너무 빨리 크롤링 하면 보안상 문제가 될 수 있으니 일정한 텀?을 가지고 돌리게하는 것.
+
+    # 리스트나 딕셔너리를 가져오고 바꾸고 개지랄하는 연습이 필요
+    # 반복문도 연습할 것.
+
+    for d in data['moreList']:
+        print(d['title'])
+        print(d['targetCo'])
+
+    #print(data['moreList'])    
+
+# 이렇게 10페이지를 가져올 수 있다.
+# 전부 다 가져오고 싶은 경우 existMore이 False가 될 때 까지 돌리면 된다.
+
+
--- 데이터수집/news_test.py
+++ 데이터수집/news_test.py
@@ -11,10 +11,14 @@ CSV: 콤마, 세퍼레이트 벨류로 나눠진다?
 from typing import AsyncGenerator
 import requests
 import json
+
+oid='001'
+aid='0012604850'
+
 params = {
     'suppress_response_codes': True
     #,‘callback’: ‘jQuery1124005705702844427485_1629261815444’
-    ,'q': 'NEWS[ne_417_0000726114]|NEWS_SUMMARY[417_0000726114]|JOURNALIST[76236(period)]|NEWS_MAIN[ne_417_0000726114]'
+    ,'q': f'''NEWS[ne_{oid}_{aid}]|NEWS_SUMMARY[417_0000726114]|JOURNALIST[76236(period)]|NEWS_MAIN[ne_417_0000726114]'''
     ,'isDuplication': False
     ,'_'0:1629261815445
 }
@@ -30,3 +34,9 @@ for d in data['contents'][0]['reactions']:
     #print(d['reactionType'])
     #print(d['count'])
 print(result)
+
+
+# 네이버 뉴스에서 oid는 언론사 코드?, aid 키값 oid+aid 이렇게 합치면 유일한 값.
+
+
+# ---------------------------------------------------------------------------------------------------------------------------
